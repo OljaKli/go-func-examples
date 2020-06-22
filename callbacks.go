93bee5f3e99cdb18f306df4f2c45ab8e9196f7a8
Daniil Maslov <i@dmaslov.dev>
2020-06-22 11:07:13 +0300

    Add err funcs

--- callbacks.go
+++ callbacks.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 func callbacks() {
 	fmt.Println(sum(3, 4, 5, 6, 7))
 
 	fmt.Println(even(sum, 3, 4, 5, 8))
+
+	fmt.Print("Someting changes")
+
+	fmt.Println("Example")
+
+	fmt.Println("Good")
 }
 
 func sum(num ...int) int {
@@ -16,10 +25,20 @@ func sum(num ...int) int {
 	}
 
 	return total
+
+	context.TODO()
 }
+
+// context.TODO()
+
+var err = r.client.Get(context.TODO(), types.NamespacedName{Name: ingressName, Namespace: instance.Namespace}, found)
+
+// TODO: dsasad
 
 func even(f func(xi ...int) int, vi ...int) int {
 	var eints []int
+
+	err := r.client.Get(context.TODO(), types.NamespacedName{Name: ingressName, Namespace: instance.Namespace}, found)
 
 	for _, v := range vi {
 		if v%2 == 0 {
